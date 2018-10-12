caa32c84e50b0484e04f09b8ab56bd5f1032f778
LashaunnaS <shaunna191@icloud.com>
2018-10-12 17:02:26 -0400

    Added transforms to each image element

--- src/components/photoGrid/PhotoGrid.jsx
+++ src/components/photoGrid/PhotoGrid.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 import { PhotoGridWrapper, Photo } from './PhotoGridStyles';
 
 const PhotoGrid = ({ imgs }) => {
+
+
+  const scaleUp = (e) => {
+    let x = e.target;
+    x.style.transform = "scale(1.2)";
+    x.style.transition = "1s";
+  }
+
+  const scaleDown = (e) => {
+    let x = e.target;
+    x.style.transform = "scale(1)";
+    x.style.transition = "1s";
+  }
+
   return (
     <PhotoGridWrapper>
       {
         imgs.map((i, index) => {
-          return <Photo key={index} src={i} />
+          return (
+            <div key={index}>
+              <Photo src={i} className={`img${index}`} onMouseOver={scaleUp} onMouseOut={scaleDown} />
+            </div>
+          )
         })
       }
+
     </PhotoGridWrapper>
   )
 
